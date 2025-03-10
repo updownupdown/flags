1c6ef2bd2dd068518c2f66eb96d02adbffcbe727
updownupdown <updownupdown@gmail.com>
2025-03-10 14:53:16 -0400

    Text fixes, clean up About modal

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -9,18 +9,22 @@ export const About = ({ onClose }: Props) => {
   return (
     <Modal title="About" isOpen onClose={onClose} modalClass="about-modal">
       <div className="about">
-        <p>Project by James Carmichael</p>
+        <h3>
+          Project by <b>James Carmichael</b>
+        </h3>
 
-        <a href="https://jamescarmichael.ca" target="_blank" rel="noreferrer">
-          See Portfolio
-        </a>
-        <a
-          href="https://github.com/updownupdown/flags"
-          target="_blank"
-          rel="noreferrer"
-        >
-          See on GitHub
-        </a>
+        <div className="about-links">
+          <a href="https://jamescarmichael.ca" target="_blank" rel="noreferrer">
+            See Portfolio
+          </a>
+          <a
+            href="https://github.com/updownupdown/flags"
+            target="_blank"
+            rel="noreferrer"
+          >
+            See on GitHub
+          </a>
+        </div>
       </div>
     </Modal>
   );
